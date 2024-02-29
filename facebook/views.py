7afa362e7380c7079f92530047f6fff03cfaa375
v2vee <nellieyvalerie@gmail.com>
2024-02-29 16:51:32 +0300

    first commit

---
+++ facebook/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+def contacts(request):
+    return render(request, 'contacts.html')
+def gallery(request):
+    return render(request, 'gallery.html')
+
+def aboutus(request):
+    return render(request, 'aboutus.html')
